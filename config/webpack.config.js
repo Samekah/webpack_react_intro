a0f0e74863df41e3701232381b4b219dcd1c713b
Tom Virgo <tomcvirgo@gmail.com>
2021-08-03 13:03:50 +0100

    change config target

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,7 +10,7 @@ const config = {
     // instructions for compiling the code
     path: path.resolve(ROOT_DIRECTORY, 'build'), // the file where the compiled code should go
     filename: "bundle.js", // the file name of the compiled code
-    publicPath: "./", // specifies the base path for all the assets within your application.
+    publicPath: "/", // specifies the base path for all the assets within your application.
   },
   mode: "development", // tells webpack to use its built-in optimizations according to the mode
   resolve: {
