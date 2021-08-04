7924910f30df11c1239cf8d53fd1347b3d0c6554
Tom Virgo <tomcvirgo@gmail.com>
2021-08-04 17:34:40 +0100

    add back button

--- src/layout/Nav/index.js
+++ src/layout/Nav/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { BackButton } from '../../components';
 import './style.css';
 
 const Nav = () => {
     return (
         <nav>
+            <BackButton></BackButton>
             <NavLink exact to="/" activeClassName="selected">Home</NavLink>
             <NavLink exact to="/Little_Simz" activeClassName="selected">Little Simz</NavLink>
             <NavLink exact to="/activity" activeClassName="selected">Random Activity</NavLink>
